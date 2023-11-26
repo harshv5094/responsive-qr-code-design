547fd0ce211ed5f8ce31a26822b33d02f60e3e3c
harshv5904 <harshv5094@gmail.com>
2023-11-26 12:51:07 +0530

    feat: Initial Commit

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import("prettier").Config} */
+
+export default {
+  tabWidth: 2,
+  arrowParens: "avoid",
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  bracketSpacing: true,
+  endOfLine: 'lf'
+}
---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="Box">
+      <div className='contentContainer'>
+        <img src="/image-qr-code.png" alt="qrCode" id="qrCode" />
+        <div className='title'>
+          Improve your frontend skills by building projects
+        </div>
+        <p>
+          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default App
